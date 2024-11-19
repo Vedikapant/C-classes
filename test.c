1f41c91b1e217f8b9132074776548a061d297557
Your Name <you@example.com>
2024-11-19 09:57:57 +0530

    positve num & seprate num

--- test.c
+++ test.c
@@ -1,11 +1,24 @@
 #include<stdio.h>
-int main ()
+int main()
 {
-int x=3,y=10,a;
- a=x;
- x=y;
- y=a; 
-printf("%d,%d",x,y);
+int x,y ;
 
-return 0;
+
+   printf("enter the number:");
+   scanf("%d",&x);
+
+  if (x>0)
+  printf("the number %d is positive",x);
+   else 
+
+ 
+y=x%100;
+ printf("x is %d",x);
+
+ return 0;
 }
+
+
+
+
+
