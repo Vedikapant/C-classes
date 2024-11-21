3d7c0a5a59d01c3c780ba41802bbc1508a73eac5
Your Name <you@example.com>
2024-11-21 09:47:11 +0530

    while loop+last commit

--- test.c
+++ test.c
@@ -1,20 +1,45 @@
 #include<stdio.h>
 int main()
 {
-int x,y ;
-
-
+int x,g,h,f,m,n;
    printf("enter the number:");
    scanf("%d",&x);
 
   if (x>0)
-  printf("the number %d is positive",x);
-   else 
+{
+  printf("the number %d is positive\n",x);
+ }
+  else
+{
+  printf("number is wrong");
+}
 
- 
-y=x%100;
- printf("x is %d",x);
+{  g=x%10;
+  printf("g is %d \n",g);
 
+  m=x/10;
+h=m%10;
+ printf("h is %d \n",h);
+
+ n=x/100;
+f=n;
+printf("f is %d \n ",f);
+
+printf("sum of num %d:\n",g+h+f);
+}
+
+{
+ while( g=x%10;h=m%10;f=n);
+{
+printf("g is %d \n",g);
+printf("h is %d \n",h);
+printf("f is %d \n",f);
+}
+
+
+
+
+}
  return 0;
 }
 
