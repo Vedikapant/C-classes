747bd714811bd171e18eafcff20c5cbc3cb2bb74
Your Name <you@example.com>
2024-11-29 09:51:43 +0530

    array assen

--- array.c
+++ array.c
@@ -1,32 +1,36 @@
-#include<stdio.h>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      #include<stdio.h>
 int main()
 {
-int i,a[5]={37,32,9,51,7},temp;
+int i,a[5]={37,32,9,51,7},temp,j;
 
-
-     for(i=0;i<5;i++)
+for(j=0;j<4;j++)
+{ 
+     for(i=j;i<5;i++)
      {
-      if(a[0]>a[i+1])
+      if(a[j]>a[i+1])
       {
-        temp=a[0];
-        a[0]=a[i+1];
+        temp=a[j];
+        a[j]=a[i+1];
         a[i+1]=temp;
       }
 
      }
 
-    for(i=0;i<4;i++)
+/*
+    for(j=2;j<4;j++)
      {
-      if(a[0]>a[i+1])
+      if(a[2]>a[j+1])
      {
-       temp=a[i];
-       a[0]=a[i+1];
-       a[i+1]=temp;
+       temp=a[2];
+       a[2]=a[j+1];
+       a[j+1]=temp;
      }
 
      }
 
-    for(i=0;i<5;i++)
+*/
+   }
+   for(i=0;i<5;i++)
   {
     printf("%d\t",a[i]);
   }
