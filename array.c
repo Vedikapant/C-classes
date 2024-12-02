68ef9d53f8e63cb730bb90458beefb58065588e6
Your Name <you@example.com>
2024-12-02 09:51:44 +0530

    array.3

--- array.c
+++ array.c
@@ -1,7 +1,7 @@
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       #include<stdio.h>
 int main()
 {
-int i,a[5]={37,32,9,51,7},temp,j;
+int i,a[5]={37,32,9,51,7},temp,j,p;
 
 for(j=0;j<4;j++)
 { 
@@ -30,9 +30,30 @@ for(j=0;j<4;j++)
 
 */
    }
+
    for(i=0;i<5;i++)
   {
     printf("%d\t",a[i]);
   }
+
+ int flag = 0;
+  printf("\nenter the value p:");
+  scanf("%d",&p);
+ for(i=0;i<5;i++){
+              if(p==a[i]){
+                          printf("the num exits\n");
+			flag = 0;
+                         break;
+                        }
+               else {
+                     flag = -1;
+                    }
+              } 
+
+if(flag == -1)
+{
+
+	printf("number doesnt' exist\n");
+}
 return 0;
 }
