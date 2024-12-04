88fc5a295319e1fb584bbc2721dfddea59eeb232
Your Name <you@example.com>
2024-12-04 09:38:18 +0530

    binary search

--- array.c
+++ array.c
@@ -1,11 +1,11 @@
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      #include<stdio.h>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    #include<stdio.h>
 int main()
 {
-int i,a[5]={37,32,9,51,7},temp,j,p;
+int i,a[10]={2,3,6,7,8,9,63,69,72,87},temp,j,p;
 
-for(j=0;j<4;j++)
-{ 
-     for(i=j;i<5;i++)
+for(j=0;j<9;j++)
+{
+     for(i=j;i<10;i++)
      {
       if(a[j]>a[i+1])
       {
@@ -31,15 +31,15 @@ for(j=0;j<4;j++)
 */
    }
 
-   for(i=0;i<5;i++)
+   for(i=0;i<10;i++)
   {
     printf("%d\t",a[i]);
   }
 
  int flag = 0;
-  printf("\nenter the value p:");
+  printf("\nenter the value of p:");
   scanf("%d",&p);
- for(i=0;i<5;i++){
+ for(i=0;i<10;i++){
               if(p==a[i]){
                           printf("the num exits\n");
 			flag = 0;
@@ -55,5 +55,28 @@ if(flag == -1)
 
 	printf("number doesnt' exist\n");
 }
+
+
+int s,low=0, high=9;
+int mid=(low+high)/2;
+
+printf("\n Enter the search element:");
+scanf("%d",&s);
+
+if(s<a[mid]) 
+            {
+              printf("the num is in right side \n");
+            }
+
+else if(s>a[mid])
+            {
+              printf("the num is in left side \n"); 
+            }
+
+else        {
+                printf("the num is in mid \n");
+             }
+
+
 return 0;
 }
